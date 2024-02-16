08afcdab4ed94b2705a6033c4c504c1e2584ce66
Shivam@8299 <shivammishra0381@gmail.com>
2024-02-16 12:36:31 +0530

    sum

---
+++ add.js
@@ -0,0 +1,9 @@
+let prompt = require('prompt-sync')();
+let num1 = prompt("enter the num1 :");
+let a= parseInt(num1);
+let num2 = prompt("enter the num2 :");
+let b = parseInt(num2);
+
+const sum = a + b;
+console.log(sum);
+
